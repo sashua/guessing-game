153ee42ba9e60a5851b7ad5ec13327eadd33b9a3
Alex Sereda <sereda.san@gmail.com>
2022-08-12 12:19:52 +0300

    feat: finish task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.min = null;
+    this.max = null;
+    this.value = null;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min + 1;
+    this.max = max - 1;
+  }
 
-    }
+  guess() {
+    this.value = Math.round((this.min + this.max) / 2);
+    return this.value;
+  }
 
-    guess() {
+  lower() {
+    this.max = this.value - 1;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.value + 1;
+  }
 }
 
 module.exports = GuessingGame;
